cf2716f23f7781b88c5cee9baa1ede06c1f157fb
hlx_glb <hlxsuccess@study.com>
2020-09-08 21:36:56 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/hlx/HelloGit.java
@@ -0,0 +1,13 @@
+package com.hlx;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author hlx
+ * @create 2020-09-08 19:38
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git");
+    }
+}
